d9f8169562eb8ac4f84cab492b97a3b5a34d5db1
EsatErgunes <esat.ergunes@student.ehb.be>
2017-05-24 14:04:50 +0200

    Product Pagina
    Rate Stars Edit

---
+++ JS/Javascript.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+  
+  $("#hide").click(function()
+  {
+    $("#filledStar1").css("display","block");
+  
+    });
+    
+  
+  
+  $("#hide2").click(function()
+  {
+    $("#filledStar1").css("display","block");
+    $("#filledStar2").css("display","block");
+  
+    });
+  
+  
+  $("#hide3").click(function()
+  {
+    $("#filledStar1").css("display","block");
+    $("#filledStar2").css("display","block");
+    $("#filledStar3").css("display","block");
+    });
+  
+  $("#hide4").click(function()
+  {
+    $("#filledStar1").css("display","block");
+    $("#filledStar2").css("display","block");
+    $("#filledStar3").css("display","block");
+    $("#filledStar4").css("display","block");
+    });
+  
+  $("#hide5").click(function()
+  {
+    $("#filledStar1").css("display","block");
+    $("#filledStar2").css("display","block");
+    $("#filledStar3").css("display","block");
+    $("#filledStar4").css("display","block");
+    $("#filledStar5").css("display","block");
+    });
+  
+
+  
+  
+  
+  
+  
+  
+  
+  $("#filledStar1").click(function()
+  {
+    $("#filledStar1").css("display","none");
+    $("#filledStar2").css("display","none");
+  
+    $("#filledStar3").css("display","none");
+     $("#filledStar4").css("display","none");
+     $("#filledStar5").css("display","none");
+    });
+  
+  
+  $("#filledStar2").click(function()
+  {
+    $("#filledStar2").css("display","none");
+  
+    $("#filledStar3").css("display","none");
+     $("#filledStar4").css("display","none");
+     $("#filledStar5").css("display","none");
+    });
+
+   $("#filledStar3").click(function()
+  {
+    $("#filledStar3").css("display","none");
+      $("#filledStar4").css("display","none");
+      $("#filledStar5").css("display","none");
+  
+    });
+  
+   $("#filledStar4").click(function()
+  {
+    $("#filledStar4").css("display","none");
+      $("#filledStar5").css("display","none");
+  
+    });
+  
+   $("#filledStar5").click(function()
+  {
+    $("#filledStar5").css("display","none");
+  
+    });
+  
+
+});
